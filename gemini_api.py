d43e7088a0c690f9d580c090c8c6b8068a8dafa3
pvure <pranay.vure@outlook.com>
2025-04-01 18:18:14 -0400

    syntax fix

--- gemini_api.py
+++ gemini_api.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyCHJhOsy12cREX8KNdCOAxrMw_L8cqQlK0")
+client = genai.Client(api_key="key")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-lite", contents="Explain how AI works in a few words"
